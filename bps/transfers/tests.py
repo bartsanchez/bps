821cb94d951dde55996ce1466f2d8242c886cfa3
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-02-20 22:53:06 +0100

    NEW: Log body content from request

--- bps/transfers/tests.py
+++ bps/transfers/tests.py
@@ -1,4 +1,6 @@
 import hashlib
+import logging
+from unittest import mock
 
 from django.test import TestCase
 from django.urls import reverse
@@ -7,13 +9,28 @@ from .models import ProcessedBulkTransfer
 
 
 class TransfersViewTests(TestCase):
+    def setUp(self):
+        self.url = reverse("bulk_transfer")
+
     def test_bulk_transfer_GET(self):
-        response = self.client.get(reverse("bulk_transfer"))
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 405)
 
     def test_bulk_transfer_POST(self):
-        response = self.client.post(reverse("bulk_transfer"))
+        response = self.client.post(self.url)
         self.assertEqual(response.status_code, 200)
+
+    def test_bulk_transfer_POST_logged(self):
+        logger = logging.getLogger("transfers.views")
+
+        msg = "{foo=1, bar=2, spam=3}"
+
+        with mock.patch.object(logger, "info") as log_mock:
+            self.client.post(self.url, data=msg, content_type="application/json")
+            log_mock.assert_called_once_with(
+                "Received content: %(content)",
+                extra={"content": msg},
+            )
 
 
 class ProcessedBulkTransferModelTests(TestCase):
--- bps/transfers/views.py
+++ bps/transfers/views.py
@@ -1,7 +1,15 @@
+import logging
+
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+
+logger = logging.getLogger(__name__)
 
 
 @require_http_methods(["POST"])
 async def bulk_transfer(request):
+    content = request.body.decode()
+
+    logger.info("Received content: %(content)", extra={"content": content})
+
     return HttpResponse("OK")
