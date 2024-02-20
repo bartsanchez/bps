838f2a0455d21a5b10bcfaba7f0df061ac92ebd6
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-02-20 22:19:49 +0100

    NEW: Use double quotes using Ruff

--- bps/bps/wsgi.py
+++ bps/bps/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bps.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bps.settings")
 
 application = get_wsgi_application()
--- bps/transfers/migrations/0001_initial.py
+++ bps/transfers/migrations/0001_initial.py
@@ -4,19 +4,25 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='ProcessedBulkTransfer',
+            name="ProcessedBulkTransfer",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('request_hash', models.CharField(max_length=64)),
-                ('content', models.TextField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("request_hash", models.CharField(max_length=64)),
+                ("content", models.TextField()),
             ],
         ),
     ]
