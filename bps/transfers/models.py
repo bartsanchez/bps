e0ef5ef887845cb51a9c0b60f3693215a456a566
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-02-20 21:59:03 +0100

    NEW: Add ProcessedBulkTransfer model

--- bps/transfers/models.py
+++ bps/transfers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import hashlib
 
-# Create your models here.
+
+class ProcessedBulkTransfer(models.Model):
+    request_hash = models.CharField(max_length=64)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.request_hash
+
+    def save(self, *args, **kwargs):
+        content = bytes(self.content, "utf-8")
+        self.request_hash = hashlib.sha256(content).hexdigest()
+        super().save(*args, **kwargs)
--- bps/transfers/tests.py
+++ bps/transfers/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+import hashlib
+from .models import ProcessedBulkTransfer
 
 
 class TransfersViewTests(TestCase):
@@ -10,3 +12,20 @@ class TransfersViewTests(TestCase):
     def test_bulk_transfer_POST(self):
         response = self.client.post(reverse("bulk_transfer"))
         self.assertEqual(response.status_code, 200)
+
+
+class ProcessedBulkTransferModelTests(TestCase):
+    def setUp(self):
+        self.processed_bulk_transfer = ProcessedBulkTransfer.objects.create(content="foo")
+
+    def test_str(self):
+        self.assertEqual(str(self.processed_bulk_transfer), hashlib.sha256(b"foo").hexdigest())
+
+    def test_request_hash_field_automatically_generated(self):
+        self.assertEqual(
+            self.processed_bulk_transfer.request_hash,
+            hashlib.sha256(b"foo").hexdigest()
+        )
+
+    def test_content(self):
+        self.assertEqual(self.processed_bulk_transfer.content, "foo")
