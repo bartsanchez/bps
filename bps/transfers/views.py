066a8143b46d7c9123d4b68074437b479999e0f5
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-02-20 23:06:08 +0100

    NEW: Ensure CSRF protection for the view

--- bps/transfers/views.py
+++ bps/transfers/views.py
@@ -1,11 +1,13 @@
 import logging
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 
 logger = logging.getLogger(__name__)
 
 
+@csrf_protect
 @require_http_methods(["POST"])
 async def bulk_transfer(request):
     content = request.body.decode()
